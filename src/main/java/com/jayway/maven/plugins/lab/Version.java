1fc7c550f8d69db513bc5cd774add12dc00d9045
Tobias Sodergren <tobias@sodergren.org>
2012-03-06 13:30:28 +0100

    Introduced BEGIN_UP_TO_VERSION

--- src/main/java/com/jayway/maven/plugins/lab/Version.java
+++ src/main/java/com/jayway/maven/plugins/lab/Version.java
@@ -4,91 +4,111 @@ package com.jayway.maven.plugins.lab;
  * Contains not only the version number, but also a flag that indicates whether
  * or not the {@link VersionedContents} should exist for exactly this version
  * only, or for all versions greater than or equal to this version.
- * 
+ *
  * @author Ulrik Sandberg
  */
 public class Version {
-	public static final Version ZERO = new Version(0);
 
-	public static final Version VERY_BIG = new Version(Integer.MAX_VALUE);
+    public enum TriggerType {
+        /**
+         * Content should only be included for a one version.
+         */
+        CONTENT_IN_THIS_VERSION_ONLY,
+        /**
+         * Content should be included from (and including) a version.
+         */
+        FROM_VERSION,
+        /**
+         * Content should be included from the first version, up to (not including) a version.
+         */
+        UP_TO_VERSION
+    }
 
-	private final int versionNumber;
+    public static final Version ZERO = new Version(0);
 
-	private final boolean contentInThisVersionOnly;
+    public static final Version VERY_BIG = new Version(Integer.MAX_VALUE);
 
-	public Version(int version) {
-		this(version, false);
-	}
+    private final int versionNumber;
 
-	public Version(int versionNumber, boolean contentOnlyInThisVersion) {
-		this.versionNumber = versionNumber;
-		this.contentInThisVersionOnly = contentOnlyInThisVersion;
-	}
+    private final TriggerType triggerType;
 
-	public int getVersionNumber() {
-		return versionNumber;
-	}
+    public Version(int version) {
+        this(version, TriggerType.FROM_VERSION);
+    }
 
-	public boolean isContentInThisVersionOnly() {
-		return contentInThisVersionOnly;
-	}
+    public Version(int versionNumber, TriggerType triggerType) {
+        this.versionNumber = versionNumber;
+        this.triggerType = triggerType;
+    }
 
-	public boolean greaterThan(Version that) {
-		return this.versionNumber > that.versionNumber;
-	}
+    public int getVersionNumber() {
+        return versionNumber;
+    }
 
-	public boolean lessThan(Version that) {
-		return this.versionNumber < that.versionNumber;
-	}
+    public TriggerType getTriggerType() {
+        return triggerType;
+    }
 
-	public boolean sameAs(int versionNumber) {
-		return this.getVersionNumber() == versionNumber;
-	}
+    public boolean greaterThan(Version that) {
+        return this.versionNumber > that.versionNumber;
+    }
 
-	public boolean sameOrLessThan(int versionNumber) {
-		return this.getVersionNumber() <= versionNumber;
-	}
+    public boolean lessThan(Version that) {
+        return this.versionNumber < that.versionNumber;
+    }
 
-	public boolean containsStuffFor(int versionNumber) {
-		if (this.isContentInThisVersionOnly()) {
-			if (this.sameAs(versionNumber)) {
-				return true;
-			}
-		}
-		else if (this.sameOrLessThan(versionNumber)) {
-			return true;
-		}
-		return false;
-	}
+    public boolean sameAs(int versionNumber) {
+        return this.getVersionNumber() == versionNumber;
+    }
 
-	@Override
-	public String toString() {
-		return "Version [contentInThisVersionOnly=" + contentInThisVersionOnly + ", versionNumber=" + versionNumber
-				+ "]";
-	}
+    public boolean sameOrLessThan(int versionNumber) {
+        return this.getVersionNumber() <= versionNumber;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (contentInThisVersionOnly ? 1231 : 1237);
-		result = prime * result + versionNumber;
-		return result;
-	}
+    public boolean greaterThan(int versionNumber) {
+        return this.getVersionNumber() > versionNumber;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Version other = (Version) obj;
-		if (contentInThisVersionOnly != other.contentInThisVersionOnly)
-			return false;
-		if (versionNumber != other.versionNumber)
-			return false;
-		return true;
-	}
+    public boolean containsStuffFor(int versionNumber) {
+        if (this.getTriggerType().equals(TriggerType.CONTENT_IN_THIS_VERSION_ONLY)) {
+            if (this.sameAs(versionNumber)) {
+                return true;
+            }
+        } else if (this.getTriggerType().equals(TriggerType.FROM_VERSION) && this.sameOrLessThan(versionNumber)) {
+            return true;
+        } else if (this.getTriggerType().equals(TriggerType.UP_TO_VERSION) && this.greaterThan(versionNumber)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Version [contentInThisVersionOnly=" + triggerType + ", versionNumber=" + versionNumber
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        int result = versionNumber;
+        result = 31 * result + (triggerType != null ? triggerType.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Version other = (Version) obj;
+        if (triggerType != other.triggerType)
+            return false;
+        if (versionNumber != other.versionNumber)
+            return false;
+        return true;
+    }
 }
